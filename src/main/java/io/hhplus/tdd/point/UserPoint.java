f897ba86d36c2a13f1ae864b13971cc4cf79a5f7
hkjs96 <hkjs96@naver.com>
2025-10-23 23:41:06 +0900

    기능: 포인트 최대 보유량 100000 제한 구현
    UserPoint.charge()에 최대 보유량 검증 로직 추가
    - 충전 후 포인트가 100000을 초과하면 MaxPointExceededException 발생

--- src/main/java/io/hhplus/tdd/point/UserPoint.java
+++ src/main/java/io/hhplus/tdd/point/UserPoint.java
@@ -11,9 +11,18 @@ public record UserPoint(
     }
 
     public UserPoint charge(long amount) {
+        final long MAX_POINT = 100_000L;
 
         if(amount < 0) {
             throw new InvalidPointAmountException(amount);
+        }
+
+        if(amount % 5_000 != 0) {
+            throw new InvalidChargeUnitException(amount, 5_000);
+        }
+
+        if(this.point + amount > MAX_POINT) {
+            throw new MaxPointExceededException(this.point, amount, MAX_POINT);
         }
 
         return new UserPoint (this.id, this.point + amount, System.currentTimeMillis());
